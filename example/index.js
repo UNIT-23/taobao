e3426b3cbdae27823195573c9e771a44d8486705
sarfraz akhtar <sarfraz.akhtar4@gmail.com>
2018-09-24 14:02:37 +0500

    fix

--- example/index.js
+++ example/index.js
@@ -2,14 +2,17 @@ require('dotenv').config()
 const taobao = require('./../taobao')
 const config = require('./config')
 
+console.log(config.config.app_key)
+console.log(config.config.app_secret)
+console.log(config.config.session)
 taobao.config({
     app_key: config.config.app_key,
-    app_secret: config.config.app_secret
+    app_secret: config.config.app_secret,
+    session: config.config.session
 });
 
-taobao.core.call({
-    method: 'taobao.areas.get',
-    fields: 'id,type,name,parent_id,zip'
+taobao.userBuyerGet({
+    fields: 'user_id,nick,sex,buyer_credit,avatar,has_shop,vip_info'
 }, function(data) {
     console.log(data);
 });
