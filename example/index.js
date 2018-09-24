eb9254ac8d8d676fb688afed505063878b82ff5d
sarfraz akhtar <sarfraz.akhtar4@gmail.com>
2018-09-24 13:04:38 +0500

    run example

--- example/index.js
+++ example/index.js
@@ -1,2 +1,15 @@
 require('dotenv').config()
+const taobao = require('./../taobao')
+const config = require('./config')
 
+taobao.config({
+    app_key: config.config.app_key,
+    app_secret: config.config.app_secret
+});
+
+taobao.core.call({
+    method: 'taobao.areas.get',
+    fields: 'id,type,name,parent_id,zip'
+}, function(data) {
+    console.log(data);
+});
